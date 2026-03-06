140fc97f9020a2159e338b5e49735d2a768db104
datapagan <joseluispagan@gmail.com>
2026-03-06 18:57:31 -0500

    Update 03_target_table.sql

--- project1-sftp-snowflake-pipeline/sql/03_target_table.sql
+++ project1-sftp-snowflake-pipeline/sql/03_target_table.sql
@@ -1,1 +1,10 @@
-
+CREATE OR REPLACE TABLE workforce_raw (
+    employee_id INT,
+    employee_name STRING,
+    department STRING,
+    job_title STRING,
+    location STRING,
+    employment_type STRING,
+    hire_date DATE,
+    salary NUMBER(10,2)
+);
