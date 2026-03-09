eaa6d26b21eb2bb09e2ec3c075b99becdf02ffff
datapagan <joseluispagan@gmail.com>
2026-03-09 12:42:23 -0400

    Update 06_audit_table.sql

--- project1-sftp-snowflake-pipeline/sql/06_audit_table.sql
+++ project1-sftp-snowflake-pipeline/sql/06_audit_table.sql
@@ -1,1 +1,9 @@
-
+------- creates an audit log table
+CREATE OR REPLACE TABLE DATA_ENGINEERING_PORTFOLIO.SFTP_PIPELINE.LOAD_AUDIT_LOG (
+    audit_id INTEGER AUTOINCREMENT START 1 INCREMENT 1,
+    file_name STRING,
+    target_table STRING,
+    load_timestamp TIMESTAMP_NTZ,
+    rows_loaded INTEGER,
+    load_status STRING
+);
