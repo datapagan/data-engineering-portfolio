fcb2290a2b9f959b5dee29e4c9382376ad792aa1
datapagan <joseluispagan@gmail.com>
2026-03-09 12:48:22 -0400

    Update 07_insert_audit_record.sql

--- project1-sftp-snowflake-pipeline/sql/07_insert_audit_record.sql
+++ project1-sftp-snowflake-pipeline/sql/07_insert_audit_record.sql
@@ -1,1 +1,14 @@
-
+INSERT INTO DATA_ENGINEERING_PORTFOLIO.SFTP_PIPELINE.LOAD_AUDIT_LOG (
+    file_name,
+    target_table,
+    load_timestamp,
+    rows_loaded,
+    load_status
+)
+SELECT
+    'workforce_headcount.csv.gz' AS file_name,
+    'WORKFORCE_RAW' AS target_table,
+    CURRENT_TIMESTAMP() AS load_timestamp,
+    COUNT(*) AS rows_loaded,
+    'SUCCESS' AS load_status
+FROM DATA_ENGINEERING_PORTFOLIO.SFTP_PIPELINE.WORKFORCE_RAW;
