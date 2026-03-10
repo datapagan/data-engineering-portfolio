7ada65a16dcaa7919fcddbdf1d8325b70b826be4
datapagan <joseluispagan@gmail.com>
2026-03-09 22:01:46 -0400

    Update 01_create_schema_objects.sql

--- project2-api-data-pipeline/sql/01_create_schema_objects.sql
+++ project2-api-data-pipeline/sql/01_create_schema_objects.sql
@@ -1,1 +1,17 @@
+-- Project 2 - API Workforce Data Pipeline
+-- Create schema-level objects for the project
 
+create database if not exists DATA_ENGINEERING_PORTFOLIO;
+create schema if not exists DATA_ENGINEERING_PORTFOLIO.API_WORKFORCE_PIPELINE;
+
+use database DATA_ENGINEERING_PORTFOLIO;
+use schema API_WORKFORCE_PIPELINE;
+
+create or replace file format FF_WORKFORCE_CSV
+    type = csv
+    skip_header = 1
+    field_optionally_enclosed_by = '"'
+    null_if = ('NULL', 'null', '');
+
+create or replace stage STG_WORKFORCE_API_FILES
+    file_format = FF_WORKFORCE_CSV;
